dde0ec000a66b3a01c55eb6fa47114894071abd8
Milin Shah <mgsh237@uky.edu>
2023-11-26 23:59:57 -0500

    Tokenized quotes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import json
+import tensorflow as tf
+#import numpy as np
+#import pandas as pd
 
 # Function to load parsed quotes from JSON
 def load_quotes_from_json(json_file_path):
@@ -21,3 +24,17 @@ print(f"Quote: {quote['quote']}")
 print(f"Author: {quote['author']}")
 print(f"Categories: {', '.join(quote['categories'])}")
 print()
+
+
+#Create list of the quotes, ignoring authors and tags
+quotesSet= []
+for quote in loaded_quotes:
+    quotesSet.append(quote['quote'])
+
+#Tokenize characters in quotes
+tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level = True)
+tokenizer.fit_on_texts(quotesSet)
+
+#Checking to see how tokenizer works on first word
+sequences = tokenizer.texts_to_sequences([quotesSet])[0]
+print(sequences)
