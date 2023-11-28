1f696dbd1919f113662c334013c4f6fa604ff8d6
BernardZach <bernardzach00@gmail.com>
2023-11-28 13:07:38 -0500

    edited ZBtesting

--- ZBtesting.py
+++ ZBtesting.py
@@ -1,12 +1,10 @@
 import numpy as np
-
+import json
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
-
-import json
 
 # Load and preprocess your data
 with open('data/quotes.json', 'r', encoding='utf-8') as jsonfile:
@@ -14,11 +12,11 @@ with open('data/quotes.json', 'r', encoding='utf-8') as jsonfile:
 
 quotes_text = [quote['quote'] for quote in quotes_data]
 
-# Tokenize the data
-tokenizer = Tokenizer()
+# Tokenize the data with a limited vocabulary size
+tokenizer = Tokenizer(num_words=500)
 tokenizer.fit_on_texts(quotes_text)
 
-total_words = len(tokenizer.word_index) + 1
+total_words = min(500, len(tokenizer.word_index) + 1)
 
 # Create input sequences and labels
 input_sequences = []
@@ -32,7 +30,6 @@ max_sequence_length = max([len(x) for x in input_sequences])
 input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
 
 X, y = input_sequences[:, :-1], input_sequences[:, -1]
-y = tf.keras.utils.to_categorical(y, num_classes=total_words)
 
 # Build an RNN model
 model = Sequential()
@@ -40,7 +37,7 @@ model.add(Embedding(total_words, 100, input_length=max_sequence_length-1))
 model.add(LSTM(100))
 model.add(Dense(total_words, activation='softmax'))
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
 model.fit(X, y, epochs=10, verbose=1)
